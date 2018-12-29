8d8df581c366cce9073b028343104c9e27ff51d0
omid stanikzai <omidstanik3@gmail.com>
2018-12-29 15:59:46 -0500

    first commit

--- src/test/java/com/MavenTest/BaseClass.java
+++ src/test/java/com/MavenTest/BaseClass.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class BaseClass {
 	@Test
 	public void setUp() {
-		System.setProperty("webdriver,chromedriver", "Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 
