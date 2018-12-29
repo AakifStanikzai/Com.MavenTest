309c11d4a30e2073455285e516154147466af742
omid stanikzai <omidstanik3@gmail.com>
2018-12-29 12:16:26 -0500

    3th push

--- src/test/java/com/MavenTest/BaseClass.java
+++ src/test/java/com/MavenTest/BaseClass.java
@@ -2,8 +2,10 @@ package com.MavenTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class BaseClass {
+	@Test
 	public void setUp() {
 		System.setProperty("webdriver,chromedriver", "Drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
