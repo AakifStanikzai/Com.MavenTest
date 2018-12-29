5ba67d05108044eec796fa364958b88fc2be60d6
omid stanikzai <omidstanik3@gmail.com>
2018-12-29 11:53:19 -0500

    second push

--- src/test/java/com/MavenTest/BaseClass.java
+++ src/test/java/com/MavenTest/BaseClass.java
@@ -1,6 +1,13 @@
 package com.MavenTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
-	
+	public void setUp() {
+		System.setProperty("webdriver,chromedriver", "Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
 
 }
